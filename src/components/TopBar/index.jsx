5daf21690665c8fcb9757e0d8949f896f52d9db2
CodeDead <admin@codedead.com>
2022-10-14 00:09:05 +0200

    Added a theme toggle

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -3,13 +3,24 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { useContext } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Brightness5Icon from '@mui/icons-material/Brightness5';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { setThemeType } from '../../reducers/MainReducer/Actions';
 import { MainContext } from '../../contexts/MainContextProvider';
 
 const TopBar = () => {
-  const [state] = useContext(MainContext);
+  const [state, d1] = useContext(MainContext);
 
-  const { languageIndex } = state;
+  const { languageIndex, themeType } = state;
   const language = state.languages[languageIndex];
+
+  /**
+   * Change the theme style
+   */
+  const changeThemeStyle = () => {
+    d1(setThemeType(themeType === 'dark' ? 'light' : 'dark'));
+  };
 
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -17,6 +28,10 @@ const TopBar = () => {
         <Typography variant="h6" noWrap component="div">
           {language.appName}
         </Typography>
+        <div style={{ flexGrow: 1 }} />
+        <IconButton color="inherit" onClick={changeThemeStyle}>
+          {themeType === 'dark' ? <Brightness5Icon /> : <Brightness7Icon />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
