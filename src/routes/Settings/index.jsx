bf7aa57603716f820312d88a87c7ffbb79f97a7b
CodeDead <admin@codedead.com>
2023-03-04 04:35:05 +0100

    Improved the settings route ui for desktop users

--- src/routes/Settings/index.jsx
+++ src/routes/Settings/index.jsx
@@ -4,7 +4,6 @@ import Typography from '@mui/material/Typography';
 import FormLabel from '@mui/material/FormLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-import Grid from '@mui/material/Grid';
 import blue from '@mui/material/colors/blue';
 import purple from '@mui/material/colors/purple';
 import deepPurple from '@mui/material/colors/deepPurple';
@@ -31,8 +30,10 @@ import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { type } from '@tauri-apps/api/os';
 import Theme from '../../components/Theme';
 import GridList from '../../components/GridList';
@@ -67,6 +68,9 @@ const Settings = () => {
   } = state;
 
   const language = state.languages[languageIndex];
+
+  const [generalExpanded, setGeneralExpanded] = useState(true);
+  const [themeExpanded, setThemeExpanded] = useState(false);
 
   const [resetDialogOpen, setResetDialogOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -138,74 +142,90 @@ const Settings = () => {
 
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} lg={12}>
-          <Typography component="h2" variant="h5" color="primary" gutterBottom>
+      <Accordion
+        expanded={generalExpanded}
+        onChange={() => setGeneralExpanded((prev) => !prev)}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography component="h2" variant="h5" color="primary">
             {language.general}
           </Typography>
-          <Card>
-            <CardContent>
-              <FormGroup>
-                {/* eslint-disable-next-line no-underscore-dangle */}
-                {window.__TAURI__ ? (
-                  <FormControlLabel
-                    control={(
-                      <Checkbox
-                        checked={autoUpdate}
-                        onChange={(e) => d1(setAutoUpdate(e.target.checked))}
-                        value="autoUpdate"
-                      />
-                      )}
-                    label={language.autoUpdate}
+        </AccordionSummary>
+        <AccordionDetails>
+          <FormGroup>
+            {/* eslint-disable-next-line no-underscore-dangle */}
+            {window.__TAURI__ ? (
+              <FormControlLabel
+                control={(
+                  <Checkbox
+                    checked={autoUpdate}
+                    onChange={(e) => d1(setAutoUpdate(e.target.checked))}
+                    value="autoUpdate"
                   />
-                ) : null}
-                <FormControlLabel
-                  control={(
-                    <Checkbox
-                      checked={colorOnDark}
-                      onChange={(e) => d1(setColorOnDark(e.target.checked))}
-                      value="colorOnDarkSelector"
-                    />
-                  )}
-                  label={language.colorOnDark}
+                )}
+                label={language.autoUpdate}
+              />
+            ) : null}
+            <FormControlLabel
+              control={(
+                <Checkbox
+                  checked={colorOnDark}
+                  onChange={(e) => d1(setColorOnDark(e.target.checked))}
+                  value="colorOnDarkSelector"
                 />
-                <FormControlLabel
-                  control={(
-                    <Checkbox
-                      checked={languageSelector}
-                      onChange={(e) => d1(setLanguageSelector(e.target.checked))}
-                      value="languageSelector"
-                    />
-                  )}
-                  label={language.languageSelector}
+              )}
+              label={language.colorOnDark}
+            />
+            <FormControlLabel
+              control={(
+                <Checkbox
+                  checked={languageSelector}
+                  onChange={(e) => d1(setLanguageSelector(e.target.checked))}
+                  value="languageSelector"
                 />
-                <FormControl variant="outlined" sx={{ mt: 2 }}>
-                  <InputLabel id="language-label">{language.language}</InputLabel>
-                  <Select
-                    value={languageIndex}
-                    onChange={handleLanguageChange}
-                    id="language-simple"
-                    labelId="language-label"
-                    label={language.language}
-                  >
-                    <MenuItem value={0}>Deutsch</MenuItem>
-                    <MenuItem value={1}>English</MenuItem>
-                    <MenuItem value={2}>Français</MenuItem>
-                    <MenuItem value={3}>日本</MenuItem>
-                    <MenuItem value={4}>Nederlands</MenuItem>
-                    <MenuItem value={5}>Русский</MenuItem>
-                    <MenuItem value={6}>中文（简体）</MenuItem>
-                  </Select>
-                </FormControl>
-              </FormGroup>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={12} lg={12}>
-          <Typography component="h2" variant="h5" color="primary" gutterBottom>
+              )}
+              label={language.languageSelector}
+            />
+            <FormControl variant="outlined" sx={{ mt: 2 }}>
+              <InputLabel id="language-label">{language.language}</InputLabel>
+              <Select
+                value={languageIndex}
+                onChange={handleLanguageChange}
+                id="language-simple"
+                labelId="language-label"
+                label={language.language}
+              >
+                <MenuItem value={0}>Deutsch</MenuItem>
+                <MenuItem value={1}>English</MenuItem>
+                <MenuItem value={2}>Français</MenuItem>
+                <MenuItem value={3}>日本</MenuItem>
+                <MenuItem value={4}>Nederlands</MenuItem>
+                <MenuItem value={5}>Русский</MenuItem>
+                <MenuItem value={6}>中文（简体）</MenuItem>
+              </Select>
+            </FormControl>
+          </FormGroup>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        TransitionProps={{ unmountOnExit: true }}
+        expanded={themeExpanded}
+        onChange={() => setThemeExpanded((prev) => !prev)}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography component="h2" variant="h5" color="primary">
             {language.theme}
           </Typography>
-
+        </AccordionSummary>
+        <AccordionDetails>
           <GridList spacing={2} xs={6} md={4} lg={4}>
             <Theme
               title={language.default}
@@ -334,8 +354,6 @@ const Settings = () => {
               onAction={() => changeTheme(17)}
             />
           </GridList>
-        </Grid>
-        <Grid item xs={12} md={12} lg={12}>
           <FormControl style={{ marginTop: 10 }} component="fieldset">
             <FormLabel component="legend">{language.themeStyle}</FormLabel>
             <RadioGroup row value={themeType} onChange={changeThemeStyle}>
@@ -343,8 +361,8 @@ const Settings = () => {
               <FormControlLabel value="dark" control={<Radio />} label={language.dark} />
             </RadioGroup>
           </FormControl>
-        </Grid>
-      </Grid>
+        </AccordionDetails>
+      </Accordion>
       {/* eslint-disable-next-line no-underscore-dangle */}
       {window.__TAURI__ ? (
         <Button
